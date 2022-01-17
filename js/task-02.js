0e89e6af6b494424710d6d0633b63f9e85bd4911
annamelya2021 <anna.basyuk@gmail.com>
2022-01-17 14:54:58 +0200

    task2new

--- js/task-02.js
+++ js/task-02.js
@@ -6,12 +6,30 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const listRef = document.querySelector('#ingredients');
 
-const listIngredienst = ingredients.map(ingredient => {
-  const newItem = document.createElement('li');
-  newItem.textContent = ingredient;
-  return newItem;
-});
 
-listRef.append(...listIngredienst);
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+const newArr= [];
+
+for (const ingredient of ingredients) {
+  const newItemLi = document.createElement("li");
+  newItemLi.textContent = ingredient;
+newItemLi.classList.add('item');
+  newArr.push(newItemLi);
+}
+
+ingredientsRef.append(...newArr);
+
+
+// const listRef = document.querySelector('#ingredients');
+
+// const listIngredienst = ingredients.map(ingredient => {
+//   const newItem = document.createElement('li');
+//   newItem.textContent = ingredient;
+//   newItem.classList.add('item');
+//   return newItem;
+// });
+
+// listRef.append(...listIngredienst);
